bfb3bbc154889790db6466fd6f4781f991b8000b
Shubhamrana99 <shubhamrana19599@gmail.com>
2024-01-19 15:45:51 +0530

    fix : signup error

--- src/context/authContext.jsx
+++ src/context/authContext.jsx
@@ -90,7 +90,7 @@ export const AuthProvider = ({ children }) => {
       signInToast();
     } catch (error) {
       console.error("Error in userSignUpDetails:", error);
-      handleErrorToast();
+      // handleErrorToast();
     }
   };
 
